8550d1e491f8d12ca4531a289aa8faf25854dd7b
David Collom <david.collom@jetstack.io>
2024-06-21 15:29:06 +0100

    Tweak init

--- kopf/nullstore/__init__.py
+++ kopf/nullstore/__init__.py
@@ -3,8 +3,6 @@ import kopf
 from .diff_base_storage import NoAnnotationsDiffBaseStorage
 from .progress_store import NullProgressStore
 
-kopf.nullstore = {}
-
 # Optionally, attach your classes to the kopf namespace if you want to make them accessible via kopf directly.
 kopf.NullProgressStore = NullProgressStore
 kopf.NoAnnotationsDiffBaseStorage = NoAnnotationsDiffBaseStorage
