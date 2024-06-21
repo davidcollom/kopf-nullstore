a227d0b66252e853e1ca35e21ba9b2208fc3a703
David Collom <david.collom@jetstack.io>
2024-06-21 15:24:54 +0100

    Auto import/setup from `kopf`

--- kopf/nullstore/__init__.py
+++ kopf/nullstore/__init__.py
@@ -1,2 +1,10 @@
+import kopf
+
 from .diff_base_storage import NoAnnotationsDiffBaseStorage
 from .progress_store import NullProgressStore
+
+kopf.nullstore = {}
+
+# Optionally, attach your classes to the kopf namespace if you want to make them accessible via kopf directly.
+kopf.NullProgressStore = NullProgressStore
+kopf.NoAnnotationsDiffBaseStorage = NoAnnotationsDiffBaseStorage
